d511a398751e3f3a02ed0afc0a421ff0d94cede9
Andrew J Wippler <andrew.wippler@gmail.com>
2014-01-18 15:40:45 -0800

    Moving files to correct directory

--- cron_regularly.php
+++ cron_regularly.php
@@ -360,61 +360,12 @@ foreach($bldgs as $b) {
 		$result_set[] = array('id' => $row[0], 'name' => $row[1], 'start' => $row[2], 'end' => $row[3], 'room' => $row[4]);
 
 		}
-		/* */
-		
-		$y = 220;
-		if (isset($result_set[0])) {
-		$im = @imagecreatefromjpeg($vertimg);
-	
-	$black = imagecolorallocate($im, 0, 0, 0);
-	$white = imagecolorallocate($im, 255, 255, 255);
-	/* See if it failed */
-	if($im) {
-		
-		
-			foreach($result_set as $bi) {
-
-		$bldg_time = date("g:i a",strtotime($bi["start"])) . " - " . date("g:i a",strtotime($bi["end"]));
-		$event_sub = substr($bi['name'], 0, 58);
-	
-					//room first
-					$x = 20;
-					$y = $y + 60; // from above. Marker is at bottom mof text.
-
-					// Write it
-					imagettftext($im, 20, 0, $x, $y, $black, $bldgfont, preg_replace('/-/', ' ',$bi['room']));
-
-					//Event subject
-					$x = $x + 140;				
-					imagettftext($im, 20, 0, $x, $y, $black, $bldgfont, $event_sub);		
 					
-					//time
-					$x = $x + 650;
-					imagettftext($im, 20, 0, $x, $y, $black, $bldgfont, $bldg_time);
-				}
-					$file = __DIR__ . "/images/".$b.".jpg";
-
-echo $file;
-				// Save the image 
-				imagejpeg($im, $file);
-		
-// Free up memory
-			imagedestroy($im);
-}
-
-}
-			
-		
-		// */
-		
-		//require($bldg_exec);
-		
-		
-		
+		require($bldg_exec);
+				
 		/* free result set */
 			mysqli_free_result($result);
 		
-
 		} else {
 			//default
 			$input = __DIR__ . "/theme/{$theme}/images/default-vertical.jpg";
@@ -427,6 +378,6 @@ echo $file;
 	}
  }
 
-
+echo $vertimg;
 
 mysqli_close($con);
