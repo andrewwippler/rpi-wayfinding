f8ce9d75fdb3181f2301c1227914ec4e1bd60eeb
Andrew J Wippler <andrew.wippler@gmail.com>
2014-02-09 14:44:27 -0800

    Resolves #10 - better puppet module

--- puppet/rpiwayfinding/manifests/init.pp
+++ puppet/rpiwayfinding/manifests/init.pp
@@ -1,17 +1,43 @@
-$webserver = 'wayfinding'
+    # Definition: rpiwayfinding
+    #
+    # This class installs RPi-Wayfinding Hosts
+    #
+    # Parameters:
+    # - The $webserver is where you want this device to point to
+    # - The $group is the floor level
+    # - The $bldg is all of the events in that location
+    #
+    # Actions:
+    # - Sets up a Feh script on the host
+    #
+    #
+    # Sample Usage:
+    #  rpiwayfinding { 'sign-name':
+    #    webserver => 'rpiwayfinding.domain.tld',
+    #  }
+    #
+    #  rpiwayfinding { 'sign-name':
+    #    template => 'bldg',
+    #    bldg => 'new',
+    #  }
+    #
 
-
-class rpiwayfinding::sign {
+define rpiwayfinding (
+$webserver = 'wayfinding',
+$group = 'first-floor',
+$bldg = 'admin',
+$template = 'sign',
+) {
 
     package { "feh":
-        ensure => latest
+        ensure => latest,
     }
 
     file { "/home/pi/rpi-wayfinding.sh":
         owner   => pi,
         group   => pi,
         mode    => 755,
-        content  => template("rpiwayfinding/rpi-wayfinding.erb"),
+        content  => template("rpiwayfinding/rpi-wayfinding-${template}.erb"),
         require => Package["feh"];
 
 	"/home/pi/.config/autostart":
@@ -40,92 +66,6 @@ class rpiwayfinding::sign {
     }
 
 
-
 }
-$area = '1st_floor'
-
-class rpiwayfinding::group {
-
-    package { "feh":
-        ensure => latest
-    }
-
-    file { "/home/pi/rpi-wayfinding.sh":
-        owner   => pi,
-        group   => pi,
-        mode    => 755,
-        content  => template("rpiwayfinding/rpi-wayfinding-group.erb"),
-        require => Package["feh"];
-
-	"/home/pi/.config/autostart":
-        ensure => directory;
-
-	"/home/pi/.config/autostart/rpi-wayfinding.desktop":
-        owner   => pi,
-        group   => pi,
-        mode    => 755,
-        source  => "puppet:///modules/rpiwayfinding/rpi-wayfinding.desktop",
-        require => Package["feh"];
-
-	"/home/pi/.config/autostart/noscreensaver.desktop":
-        owner   => pi,
-        group   => pi,
-        mode    => 755,
-        source  => "puppet:///modules/rpiwayfinding/noscreensaver.desktop",
-        require => Package["feh"];
-    
-	"/home/pi/noscreensaver.sh":
-        owner   => pi,
-        group   => pi,
-        mode    => 755,
-        source  => "puppet:///modules/rpiwayfinding/noscreensaver.sh",
-        require => Package["feh"];
-    }
-
-
-}
-
-$bldg = 'administration'
-class rpiwayfinding::bldg {
-
-    package { "feh":
-        ensure => latest
-    }
-
-    file { "/home/pi/rpi-wayfinding.sh":
-        owner   => pi,
-        group   => pi,
-        mode    => 755,
-        content  => template("rpiwayfinding/rpi-wayfinding-bldg.erb"),
-        require => Package["feh"];
-    
-	"/home/pi/.config/autostart":
-        ensure => directory;
-
-	"/home/pi/.config/autostart/rpi-wayfinding.desktop":
-        owner   => pi,
-        group   => pi,
-        mode    => 755,
-        source  => "puppet:///modules/rpiwayfinding/rpi-wayfinding.desktop",
-        require => Package["feh"];
-
-	"/home/pi/.config/autostart/noscreensaver.desktop":
-        owner   => pi,
-        group   => pi,
-        mode    => 755,
-        source  => "puppet:///modules/rpiwayfinding/noscreensaver.desktop",
-        require => Package["feh"];
-    
-	"/home/pi/noscreensaver.sh":
-        owner   => pi,
-        group   => pi,
-        mode    => 755,
-        source  => "puppet:///modules/rpiwayfinding/noscreensaver.sh",
-        require => Package["feh"];
-    }
-
-}
-
-
 
 
