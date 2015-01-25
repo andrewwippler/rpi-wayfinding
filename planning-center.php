7acf07d83b9d6269068e4a224713b60677e92272
Andrew Wippler <andrew.wippler@lancasterbaptist.org>
2015-01-24 16:27:28 -0800

    fixing planning center error entries

--- planning-center.php
+++ planning-center.php
@@ -27,14 +27,17 @@ $dom->preserveWhiteSpace = false;
 
 foreach ($dom->getElementsByTagName('tr') as $e) {
 	$ex = explode("     ",$e->nodeValue);
+	
 	$array[] = array("time" => preg_replace('!\s+!', ' ', $ex[0]), "subj" => preg_replace('!\s+!', ' ', $ex[2]));
 }
 
 //remove the first 2 values (we don't need them)
 $array = array_slice($array, 2); 
 
+
 //Parse array
 foreach ($array as $o) {
+    if (strlen($o['time']) > 5) {
 	$time = explode(" - ", $o['time']);
     $endtime = $time[1];
     $starttime = $time[0];
@@ -45,9 +48,9 @@ foreach ($array as $o) {
       //assuming still connected to database
     $query = "INSERT INTO events (EventName, Start, End, Room, Grp, Bldg) VALUES (?,?,?,?,?,?)";
     $stmt = mysqli_prepare($con, $query);
-    mysqli_stmt_bind_param($stmt, "ssssss", $title, $starttime, $endtime, $r['name'], $r['group'], $r['bldg']);
+    mysqli_stmt_bind_param($stmt, "ssssss", $title, $starttime2, $endtime2, $r['name'], $r['group'], $r['bldg']);
     // Execute the statement 
     mysqli_stmt_execute($stmt);
-  
-
+    unset($array);
+    }
 }
