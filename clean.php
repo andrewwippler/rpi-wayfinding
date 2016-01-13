be798dba5c75ae849c067ff9c63d25f9e26ddf97
Andrew Wippler <andrew.wippler@gmail.com>
2016-01-13 05:57:44 -0800

    updating lines to include error information.

--- clean.php
+++ clean.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * This file is intended to remove duplicates from the database. A duplicate is the same event in the same room typically caused by all-day events.
@@ -7,18 +7,18 @@
 
 //SELECT DISTINCT *, COUNT(*) AS fieldCount FROM  rpiwayfinding GROUP BY EventName, Room, Bldg HAVING fieldCount > 1;
 
-$query = "DELETE FROM rpiwayfinding USING rpiwayfinding, rpiwayfinding AS vtable 
-		  WHERE (rpiwayfinding.PID > vtable.PID) 
-			AND (rpiwayfinding.EventName=vtable.EventName) 
+$query = "DELETE FROM rpiwayfinding USING rpiwayfinding, rpiwayfinding AS vtable
+		  WHERE (rpiwayfinding.PID > vtable.PID)
+			AND (rpiwayfinding.EventName=vtable.EventName)
 			AND (rpiwayfinding.Room=vtable.Room)
 			AND (rpiwayfinding.Bldg=vtable.Bldg);";
-$stmt = mysqli_prepare($con, $query);
+$stmt = mysqli_prepare($con, $query) or die(mysqli_error($con));
 mysqli_stmt_execute($stmt);
 
 //SELECT * FROM events WHERE End <= NOW()
 
 $query2 = "DELETED FROM events WHERE End <= '".date("Y-m-d H:i:s")."'";
-$stmt2 = mysqli_prepare($con, $query2);
+$stmt2 = mysqli_prepare($con, $query2) or die(mysqli_error($con));
 mysqli_stmt_execute($stmt2);
 
 
